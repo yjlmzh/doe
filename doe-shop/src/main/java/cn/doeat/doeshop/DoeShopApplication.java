1131fc13ea576bc2a2665615a7f09c160560d463
aiguoba <873445182@qq.com>
2019-04-22 14:11:36 +0800

    Squashed commit of the following:
    commit 6dd9a3814ea7f20dc8c28d3be2539815be1f5415
    Author: aiguoba <873445182@qq.com>
    Date:   Mon Apr 22 12:32:30 2019 +0800
    
        first version

---
+++ doe-shop/src/main/java/cn/doeat/doeshop/DoeShopApplication.java
@@ -0,0 +1,13 @@
+package cn.doeat.doeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoeShopApplication.class, args);
+    }
+
+}
